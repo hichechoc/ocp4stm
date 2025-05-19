93f6a90727c361204de844ad0716b419d6c79540
hichechoc <hichechoc@gmail.com>
2025-05-19 16:06:15 +0200

    Add files via upload

--- democonfiguration/admin/policy-server/init.d/deployed/ssl_config.sh
+++ democonfiguration/admin/policy-server/init.d/deployed/ssl_config.sh
@@ -1,1 +1,12 @@
-/
+#!/bin/bash
+echo "[POLICY_SERVER_CONTAINER] SSL Configuration Information"
+sslentryfile=/opt/CA/siteminder/ssl_info.ini
+cat $sslentryfile
+echo "[POLICY_SERVER_CONTAINER] Native Driver- MySQL Datbase inforamtion"
+cat /opt/CA/siteminder/native/odbc/etc/system_odbc.ini
+sed -i "/OPTION=2/r $sslentryfile" /opt/CA/siteminder/native/odbc/etc/system_odbc.ini
+echo "[POLICY_SERVER_CONTAINER] Native Driver- MySQL Datbase inforamtion with SSL enabled"
+echo "**********"
+cat /opt/CA/siteminder/native/odbc/etc/system_odbc.ini
+echo "***********"
+echo "[POLICY_SERVER_CONTAINER] SSL is enabled  successfully for MySQL Datase"
